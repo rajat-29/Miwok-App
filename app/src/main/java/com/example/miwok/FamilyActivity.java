b3f9d32fcf90cd74a6574d04b392417a3052c8cf
rajat-29 <rajat295gupta@gmail.com>
2019-05-21 10:40:54 +0530

    audio buttons

--- app/src/main/java/com/example/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/miwok/FamilyActivity.java
@@ -61,6 +61,13 @@ public class FamilyActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
